ba3ef50ca1d19d64a1eb5afa91da03bfb835d212
md0924664-cmyk <md0924664@gmail.com>
2026-06-24 09:30:34 +0530

    commit

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+print("My NAme IS Mihir Darji")
+print("My Favorite App Is Instagram")
+
+print("Welcome to Data Analytics with Python!")
+
+import datetime
+
+current_year = datetime.datetime.now().year
+print("current year is", current_year)
