dc37313b6005d06dc3eceaf4d4204e90c6c15534
md0924664-cmyk <md0924664@gmail.com>
2026-07-01 14:43:16 +0530

    commit

---
+++ Assignment2.py
@@ -0,0 +1,84 @@
+# 1.Create three variables in Python: user_name, fav_app, and daily_usage_hours. Assign your own name, 
+    # your favorite app (like Instagram or Zomato), and how many hours you use it daily.
+
+user_name="Mihir"
+fav_app="Instagram"
+daily_usage_hours=4
+
+print(user_name)
+print(fav_app)
+print(daily_usage_hours)
+
+#-----------------------------------------------------------------------------------------------------------------------
+ 
+ # 2.Write a Python script that declares variables for product_name, price, and is_available to represent an item on Flipkart. 
+ # Print each variable and its data type using the type() function.
+
+product_name="Book"
+price=500
+is_available=True
+
+print(product_name)
+print(type(product_name))
+
+print(price)
+print(type(price))
+
+print(is_available)
+print(type(is_available))
+
+#------------------------------------------------------------------------------------------------------------------------
+ 
+ #3.Demonstrate the difference between single-line and multi-line comments in Python by writing a script that explains 
+ # how a Spotify playlist recommendation system might work.Use # for single-line and triple quotes for multi-line comments.
+
+
+# Spotify Recommendation System
+
+# User listens to songs.
+# Python checks the user's favorite songs.
+
+"""
+Spotify Recommendation System
+
+1. Check the user's listening history.
+2. Find similar songs.
+3. Recommend new songs to the user.
+"""
+
+print("Spotify recommends songs based on your listening history.")
+
+
+#------------------------------------------------------------------------------------------------------------------------
+ 
+ #4.Create variables for order_total, delivery_region, and discount_percent to represent a Zomato order. 
+ # Follow Python naming conventions and print a sentence using all three variables, like 
+ # 'Order from [region] totals ₹[order_total] with [discount_percent]% discount.'
+
+ordet_total = 400
+delivery_region = "Ahmedabad"
+discount_percent = 10
+
+print("Order from ",delivery_region," totals ₹",ordet_total," with ",discount_percent,"% discount.")
+
+#------------------------------------------------------------------------------------------------------------------------
+
+ # 5.Write a Python script that intentionally mixes tabs and spaces for indentation, then fix the script so it runs without 
+ # errors.<br><br><em><strong>Hint:</strong> Use only spaces for indentation, as per Python's best practices.</em>
+
+# With Error of Mixes tab & spaces for indentations
+         #    x = 10
+         #         print(x)
+
+# Without Error of indentations
+x=10
+print(x)
+    
+
+
+
+
+
+
+
+
